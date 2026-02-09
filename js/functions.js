a0b2757f417ebfb1aed374e41b330e46ea1f0c8e
goldenboyxo <130670355+goldenboyxo@users.noreply.github.com>
2026-02-08 23:59:23 -0500

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -100,20 +100,20 @@ function timeElapse(date){
 	seconds = seconds % (3600 * 24);
 	
 	var hours = Math.floor(seconds / 3600);
-	//if (hours < 10) {
-	//	hours = "0" + hours;
-	//}
+	if (hours < 10) {
+		hours = "0" + hours;
+	}
 	
 	seconds = seconds % 3600;
 	var minutes = Math.floor(seconds / 60);
-	//if (minutes < 10) {
-	//	minutes = "0" + minutes;
-	//}
+	if (minutes < 10) {
+		minutes = "0" + minutes;
+	}
 	
 	seconds = seconds % 60;
-	//if (seconds < 10) {
-	//	seconds = "0" + seconds;
-	//}
+	if (seconds < 10) {
+		seconds = "0" + seconds;
+	}
 	
 	var result = 
 		"<span class=\"digit\">" +
