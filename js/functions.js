18ff7df1f98dacca4fc89e727dd051ce28a6bebe
goldenboyxo <130670355+goldenboyxo@users.noreply.github.com>
2026-02-08 23:17:11 -0500

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -96,33 +96,21 @@ function timeElapse(date){
 	var current = Date();
 	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
 	var days = Math.floor(seconds / (3600 * 24));
-	// --- YEARS ---
-	var years = Math.floor(days / 365.25);
-	days = Math.floor(days % 365.25);
 	seconds = seconds % (3600 * 24);
 	var hours = Math.floor(seconds / 3600);
 	if (hours < 10) {
-	hours = "0" + hours;
+		hours = "0" + hours;
 	}
-
 	seconds = seconds % 3600;
 	var minutes = Math.floor(seconds / 60);
 	if (minutes < 10) {
 		minutes = "0" + minutes;
 	}
-
-	seconds = Math.floor(seconds % 60);
+	seconds = seconds % 60;
 	if (seconds < 10) {
 		seconds = "0" + seconds;
 	}
-
-	var result =
-		"<span class=\"digit\">" + years + "</span> years " +
-		"<span class=\"digit\">" + days + "</span> days " +
-		"<span class=\"digit\">" + hours + "</span> hours " +
-		"<span class=\"digit\">" + minutes + "</span> minutes " +
-		"<span class=\"digit\">" + seconds + "</span> seconds";
-
+	var result = "<span class=\"digit\">" + days + "</span> days <span class=\"digit\">" + hours + "</span> hours <span class=\"digit\">" + minutes + "</span> minutes <span class=\"digit\">" + seconds + "</span> seconds"; 
 	$("#elapseClock").html(result);
 }
 
